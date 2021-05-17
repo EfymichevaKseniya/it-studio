4c7c401e4b64f65b4aca1f14ba1e67af266f94ae
Ефимычева Ксения <cocraren359@gmail.com>
2021-05-17 16:55:00 +0400

    переделала мобильное меню

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-const navList = document.querySelector('.nav__list'),
-        mobileBtn = document.querySelector('.mobile__menu');
+const   navList = document.querySelector('.nav__list'),
+        mobileBtn = document.querySelector('.mobile__menu'),
+        nav = document.querySelector('.nav'),
+        navHeight = nav.offsetTop;
 
     mobileBtn.addEventListener('click', (e) => {
         e.preventDefault();
 
         navList.classList.toggle('show');
+        mobileBtn.classList.toggle('close');
     });
+
+
+function menuFixed() {
+    window.addEventListener('scroll', (e) => {
+        e.preventDefault();
+    
+        if (window.scrollY > 20) {
+            nav.classList.add('fixed');
+        } else {
+            nav.classList.remove('fixed');
+        }
+    });
+}
+
+menuFixed();
+
+
+
